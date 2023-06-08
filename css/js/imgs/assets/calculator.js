c64247527f57e335fb8cad5965d59dc8837c2f61
XimiPali <q.palinyc@gmail.com>
2023-06-07 21:20:39 -0400

    day 3

---
+++ css/js/imgs/assets/calculator.js
@@ -0,0 +1,11 @@
+function display(data){
+    document.getElementById('screen').value +=data;
+    return data;
+}
+function solve(){
+    let screen = document.getElementById('screen');
+    let data = screen.value;
+    let result = eval(data);
+    screen.value= result;
+    return result;
+}
